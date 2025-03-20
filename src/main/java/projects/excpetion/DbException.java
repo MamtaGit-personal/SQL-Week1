8923bd634a32a8b30c2bcee0c3127ad386efa321
Mamta Nath <mamtanathster@gmail.com>
2025-03-20 14:17:41 -0700

    MySQL week 1 project

---
+++ src/main/java/projects/excpetion/DbException.java
@@ -0,0 +1,19 @@
+package projects.excpetion;
+
+public class DbException extends RuntimeException {
+
+	public DbException() {
+	}
+
+	public DbException(String message) {
+		super(message);
+	}
+
+	public DbException(Throwable cause) {
+		super(cause);
+	}
+
+	public DbException(String message, Throwable cause) {
+		super(cause);
+	}
+}
